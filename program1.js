7d1ae9e3c334988fff0a85a33b2a80fe9c043fe0
neerajmargare <neerajmargare@gmail.com>
2024-07-26 13:13:54 +0530

    Adding file

--- program1.js
+++ program1.js
@@ -1,8 +1,48 @@
-const getTotalIsles = function (grid) {
+const getTotalIsles = function(grid) {
+  if (grid.length === 0) return 0;
+  
+  const rows = grid.length;
+  const cols = grid[0].length;
+  const visited = Array.from({ length: rows }, () => Array(cols).fill(false));
 
+  const dfs = (row, col) => {
+      
+      const directions = [
+          [-1, 0], // up
+          [1, 0],  // down
+          [0, -1], // left
+          [0, 1]   // right
+      ];
+      
+      
+      const stack = [[row, col]];
+      while (stack.length > 0) {
+          const [currentRow, currentCol] = stack.pop();
+          if (currentRow < 0 || currentRow >= rows || currentCol < 0 || currentCol >= cols || grid[currentRow][currentCol] === 'W' || visited[currentRow][currentCol]) {
+              continue;
+          }
+          visited[currentRow][currentCol] = true;
+         
+          for (const [rOffset, cOffset] of directions) {
+              stack.push([currentRow + rOffset, currentCol + cOffset]);
+          }
+      }
+  };
 
-  // write your code here
+  let islandCount = 0;
 
+ 
+  for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+          if (grid[i][j] === 'L' && !visited[i][j]) {
+              
+              dfs(i, j);
+              islandCount++;
+          }
+      }
+  }
+
+  return islandCount;
 };
 
 module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -1,7 +1,32 @@
-const decodeTheRing = function (s, p) {
+function decodeTheRing(message, key) {
+  const mLen = message.length;
+  const kLen = key.length;
 
-    // write your code here
+  const dp = Array(mLen + 1).fill(false).map(() => Array(kLen + 1).fill(false));
 
-  };
-  
-  module.exports = decodeTheRing;
+  dp[0][0] = true;
+
+  for (let j = 1; j <= kLen; j++) {
+    if (key[j - 1] === '*') {
+      dp[0][j] = dp[0][j - 1];
+    }
+  }
+
+  for (let i = 1; i <= mLen; i++) {
+    for (let j = 1; j <= kLen; j++) {
+      if (key[j - 1] === message[i - 1] || key[j - 1] === '?') {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else if (key[j - 1] === '*') {
+        dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+      }
+    }
+  }
+
+  return dp[mLen][kLen];
+}
+
+// Test cases
+// console.log(decodeTheRing("aa", "*"));      // true
+// console.log(decodeTheRing("cb", "?a"));     // false
+
+module.exports = decodeTheRing;
